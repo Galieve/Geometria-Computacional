2e8c2f9b5b3fe160fd941463ca30050cf0e9a328
Galieve <enriqueroman@live.com>
2020-02-20 18:31:53 +0100

    plot function done it!

--- 1/1.py
+++ 1/1.py
@@ -1,11 +1,14 @@
 from collections import OrderedDict
 import numpy as np
 import random
+from matplotlib import pyplot as plt
 
 
 def f(x, r):
     return r * x * (1 - x)
-# f(x) = r*x - r*x**2; f'(x) = r*(1-2*x) <= r => no contractiva
+
+
+# f(x) = r*x - r*x**2; |f'(x)| = |r*(1-2*x)| <= r => no contractiva (siempre)
 
 
 def eps():
@@ -69,17 +72,33 @@ def orbits_equivalent(o1, o2):
     return True
 
 
-
 def is_atractor(f, x0):
     m, k = floyd_cycle_finding(x0, f)
     l = get_partial_orbit(f, m, k, x0)
-    for delta in np.arange(eps(), eps()/100, -eps()/100):
+    for delta in np.arange(eps(), eps() / 100, -eps() / 100):
         md, kd = floyd_cycle_finding(x0 + delta, f)
         ld = get_partial_orbit(f, md, kd, x0)
         if not orbits_equivalent(l, ld):
             return False
     return True
 
+
+def plot_function(fr, x0, m, k):
+    orb = get_partial_orbit(fr, 0, m + k, x0)
+    x = orb[: len(orb) - 1]
+    y = orb[1:]
+    plt.plot(x, y, color='teal')
+
+    plt.plot(x[0], y[0], 'o', color='goldenrod')
+
+    atractor = get_partial_orbit(fr, m, k, x0)
+    x = atractor
+    y = atractor[1:]
+    y = np.append(y, fr(x[-1]))
+    plt.scatter(x, y, color='violet')
+
+
+    plt.show()
 
 def exercise1():
     l = []
@@ -89,13 +108,14 @@ def exercise1():
         fr = lambda x0: f(x0, r)
         if is_atractor(fr, x0):
             m, k = floyd_cycle_finding(x0, fr)
-            l.append((x0, r, get_partial_orbit(fr, m, k, x0)))
+            l.append((x0, r, get_partial_orbit(fr, m, k, x0), m , k))
     for i in l:
-        x0, r, li = i
-        print("Orbit with x0:", x0, "r:", r, "set:", li)
+        x0, r, li, m, k = i
+        print("Orbit with r:", r, "x0:", x0, "set:", li)
+        plot_function(fr, x0, m, k)
+
 
 def exercise2():
-
     x0 = random.uniform(0, 1)
     orb = None
     found = False
@@ -108,10 +128,9 @@ def exercise2():
             if len(orb) == 8:
                 found = True
 
-    print("Orbit with x0:", x0, "r:", r, "set:", orb)
+    print("Orbit with r:", r, "x0:", x0, "set:", orb)
 
 
 if __name__ == "__main__":
-
-  exercise1()
-  exercise2()
+    exercise1()
+    exercise2()
