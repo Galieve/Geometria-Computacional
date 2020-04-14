5265a5d846d0df0589fdd65d4a450e2d6c81cb78
Galieve <enriqueroman@live.com>
2020-04-15 00:00:15 +0200

    5 & 5-extra with comments

--- 5-extra/5-extra.py
+++ 5-extra/5-extra.py
@@ -5,59 +5,54 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib import animation
 
-
+# Nos colocamos en la carpeta de este archivo.
 def setup():
     workpath = os.path.dirname(os.path.abspath(__file__))
     os.chdir(workpath)
 
 
-def eps():
-    return 1e-6
-
-
-# r(z) = 1/(1 - z) si z >= 0
-# r(z) = 1 + z si z <= 0
-# theta = atan2(x, y)
-def transform_curve(x, y, z, t, alpha=0.1):
-    theta = np.arctan2(y, x)
-    r = np.array([1 - i if i >= 0 else (1 / (1 + i) ** alpha if i > -1 else 0) for i in z])
-    return (1 - t) * x + t * (r * np.sin(theta)), (1 - t) * y + t * (r * np.cos(theta)), (1 - t) * z
-
-
-def transform_surface(x, y, z, t, alpha=0.1):
-    theta = np.arctan2(y, x)
-    r = np.array([[1 - i if i >= 0 else (1 / (1 + i) ** alpha if i > -1 else 0) for i in j] for j in z])
-    return (1 - t) * x + t * (r * np.sin(theta)), (1 - t) * y + t * (r * np.cos(theta)), (1 - t) * z
-
-def transform_parametric(x, y, z, t):
-    l = (1 - t) + np.tan(t * np.arctan(abs(-1 - z)))
+# Devolvemos f(x, y, z, t), donde (x, y, z) estan en
+# S2 \ {(0,0,-1)} y t en [0, 1].
+def transform_tan(x, y, z, t):
+    l = (1 - t) + np.tan(t * np.arctan(1 + z))
     return x / l, y / l, -t + z * (1 - t)
 
 
+# Dado un tiempo t, una colección de puntos de la superficie
+# (x, y, z) y de la curva (x_, y_, z_) calculamos f_t para
+# cada uno de ellos.
 def animate(x, y, z, x_, y_, z_, t):
-    alpha = 0.2
-    # xt, yt, zt = transform_surface(x, y, z, t, alpha)
-    # x_t, y_t, z_t = transform_curve(x_, y_, z_, t, alpha)
-    xt, yt, zt = transform_parametric(x, y, z, t)
-    x_t, y_t, z_t = transform_parametric(x_, y_, z_, t)
+
+    xt, yt, zt = transform_tan(x, y, z, t)
+    x_t, y_t, z_t = transform_tan(x_, y_, z_, t)
 
     ax = plt.axes(projection='3d')
+
+    # Fijamos los ejes para ver fielmente la deformación.
+    ax.set_xlim3d(-1, 1)
+    ax.set_ylim3d(-1, 1)
     ax.set_zlim3d(-1, 1)
+
+    # Pintamos la superficie deformada en tiempo t.
     ax.plot_surface(xt, yt, zt, rstride=1, cstride=1,
                     cmap='jet', edgecolor='none')
 
+    # Pintamos la curva gamma deformada en tiempo t.
     ax.plot(x_t, y_t, z_t, '-b', c="white", zorder=3)
     return ax,
 
 
+# Ejercicio 3
 def exercise3():
+    # Parametrización discreta de la esfera.
     u = np.linspace(0, np.pi, 30)
-    v = np.linspace(0, 2 * np.pi, 50)
+    v = np.linspace(0, 2 * np.pi, 60)
 
     x = np.outer(np.sin(u), np.sin(v))
     y = np.outer(np.sin(u), np.cos(v))
     z = np.outer(np.cos(u), np.ones_like(v))
 
+    # Parametrización discreta de la curva gamma.
     t2 = np.linspace(-1, 0, 500)
 
     y2 = t2 * np.sin(127 * t2 / 2)
@@ -69,13 +64,13 @@ def exercise3():
     b2 = t2 * np.cos(127 * t2 / 2)
     z2 = np.concatenate((z2, -b2))
     x2 = np.concatenate((x2, -np.sqrt(1 - a2 ** 2 - b2 ** 2)))
-    c2 = x2 + y2
 
+    # Generamos la animacion.
     fig = plt.figure(figsize=(6, 6))
     ani = animation.FuncAnimation(fig, lambda t: animate(x, y, z, x2, y2, z2, t),
                                   np.arange(0, 1, 0.0125), interval=80)
 
-    ani.save("projected_sphere_new_version.htm", fps=10)
+    ani.save("projected_sphere_tangent_version.gif", fps=10)
 
 
 if __name__ == "__main__":
--- 5/5.py
+++ 5/5.py
@@ -14,67 +14,26 @@ from mpl_toolkits.mplot3d import axes3d
 from matplotlib import animation
 
 
-# z / d = tan x, x in (-90, 90)
-
-# (x, y, z) = (d sin t, d cos t, z), donde d**2 + z**2 = 1 (d = +sqrt(x**2 + y**2))
-# (d sin t, d cos t, z) -> (sin t, cos t, z / d) = (sin t, cos t, h) (cont en S2 \ polos)
-# (sin t, cos t, h) -> r = (h + 1), (r sin t, r cos t) si h >= 0.
-# (sin t, cos t, h) ->  r = 1/(1 - h), (r sin t, r cos t) si h <= 0.
-
-# (x, y, z) = (d sin t, d cos t, z) -> (sin t, cos t, tan x) -> ((z/d + 1) sin t, (z/d + 1) cos t), z >= 0.
-# (x, y, z) = (d sin t, d cos t, z) -> (sin t, cos t, tan x) -> ((d/(d - z) sin t, (d/(d - z) cos t), z <= 0.
-
-# (x, y, z) = (d sin t, d cos t, z) -> (sin t, cos t, tan x) -> ((tan x + 1) sin t, (tan x + 1) cos t), z >= 0.
-# (x, y, z) = (d sin t, d cos t, z) -> (sin t, cos t, tan x) -> ((1/(1 - tan x) sin t, (1/(1 - tan x) cos t), z <= 0.
-
-
-# p: (0, 1) -> S1\{(0,1)} -> R
-# t -> (sen t, cos t) -> sen t/(1 - cos t)
-# s' = p-1, R -> (0,1)
-# s(x) = (s'(x)-0.5)*pi, s: R -> (-pi/2, pi/2)
-
-# tan(x/(1-z)) no esta bien definida.
-# f(x,y,z) = (tan(s(x/(1-z))), tan(s(y/(1-z))))
-
-# f(x,y,z) = (tan(x/(1-z)), tan(y/(1-z))
-# BIEN DEFINIDA
-# x/(1-z) = +-pi/2 no pasa
-# x = (1-z)*(pi/2)
-# y**2 = 1 - (pi/2)**2*(1-z)**2 - z**2 = g(z)
-# ¿g(z) > 0? Si, a veces.
-
-
-# SOBREYECTIVA
-# sean a, b \in R, queremos x, y, z/ x**2+y**2+z**2=1 y f(x,y,z) = (a,b)
-# c = arctan(a), d = arctan(b), s(x) = c*(1-z), s(y) = d*(1-z)
-# saldrá...
-# x**2 + y**2 + z**2 = 1 =>
-# c**2*(1-s(z))**2 + d**2*(1-z)**2 + z**2 =>
-# g(s(z)) = z**2(1 + c**2 + d**2) - 2*z*(c**2 + d**2) + (c**2 + d**2 - 1) = 0
-# discr(g) = 4(c**2 + d**2)**2 - 4*(c**2 + d**2 + 1)*(c**2 + d**2 - 1) =
-# 4
-# z = (-b +-2)/2*a) = ((c**2+d**2+-1)/(c**2+d**2 + 1) =>
-# z = 1, 1 - 2/(c**2+d**2 + 1), pero 1 NO VALE
-
-# INYECTIVA
-# sea f(x,y,z) = (a,b), f(x',y',z') = (a,b) => (x,y,z) = (x', y', z')
-#  a = tan(x/(1-z)), b = tan(y/(1-z))
-
-
+# Nos colocamos en la carpeta de este archivo.
 def setup():
     workpath = os.path.dirname(os.path.abspath(__file__))
     os.chdir(workpath)
 
 
-def proj(x, z, z0=1, alpha=1):
+# Proyectamos la componente x de la esfera usando como eje
+# proyectante el z. z0 = -1 ya que el polo que vamos a
+# eliminar va a ser el (0,...0,-1).
+def proj(x, z, z0=-1, alpha=1):
     z0 = z * 0 + z0
     eps = 1e-16
+    # Nótese que añadimos un épsilon para evitar dividir entre 0.
     x_trans = x / (abs(z0 - z) ** alpha + eps)
     return (x_trans)
-    # Nótese que añadimos un épsilon para evitar dividi entre 0!!
 
 
+# Ejercicio 1
 def exercise1():
+    # Parametrización discreta de la esfera.
     u = np.linspace(0, np.pi, 25)
     v = np.linspace(0, 2 * np.pi, 50)
 
@@ -82,8 +41,8 @@ def exercise1():
     y = np.outer(np.sin(u), np.cos(v))
     z = np.outer(np.cos(u), np.ones_like(v))
 
+    # Parametrización discreta de la curva gamma.
     t2 = np.linspace(-1, 0, 500)
-
     y2 = t2 * np.sin(127 * t2 / 2)
     z2 = t2 * np.cos(127 * t2 / 2)
     x2 = np.sqrt(1 - z2 ** 2 - y2 ** 2)
@@ -94,51 +53,78 @@ def exercise1():
     b2 = t2 * np.cos(127 * t2 / 2)
     z2 = np.concatenate((z2, -b2))
     x2 = np.concatenate((x2, -np.sqrt(1 - a2 ** 2 - b2 ** 2)))
-    c2 = x2 + y2
 
+    # Generamos la figura.
     fig = plt.figure()
     ax = plt.axes(projection='3d')
+
+    ax.set_xlim3d(-1, 1)
+    ax.set_ylim3d(-1, 1)
+    ax.set_zlim3d(-1, 1)
+
+    # Pintamos la esfera.
     ax.plot_surface(x, y, z, rstride=1, cstride=1,
                     cmap='jet', edgecolor='none')
+    # Pintamos la curva.
     ax.plot(x2, y2, z2, '-b', c="white", zorder=3)
     ax.set_title('2-sphere');
     plt.savefig("2-sphere.png")
     # plt.show()
     plt.close(fig)
 
-    # NUEVO
+    # Generamos la figura proyectada.
     fig = plt.figure()
     ax = plt.axes(projection='3d')
+    ax.set_zlim3d(-1, 1)
+    # No fijamos el resto de ejes ya que sino no se ve
+    # adecuadamente la figura. Para un análisis en este sentido,
+    # el ejercicio siguiente
 
-    ax.plot_surface(proj(x, z), proj(y, z), z * 0 + 1, rstride=1, cstride=1,
-                    cmap='jet', edgecolor='none')
-    ax.plot(proj(x2, z2), proj(y2, z2), 1, '-b', c="white", zorder=3)
+    # Pintamos la esfera proyectada sobre el plano z = -1.
+    ax.plot_surface(proj(x, z), proj(y, z), z * 0 -1, rstride=1,
+                    cstride=1, cmap='jet', edgecolor='none')
+
+    # Pintamos la curva gamma sobre el plano z = -1.
+    ax.plot(proj(x2, z2), proj(y2, z2), -1, '-b', c="white", zorder=3)
     ax.set_title('Stereographic projection');
     # plt.show()
     plt.savefig("Stereographic projection.png");
     plt.close(fig)
 
 
+# Devolvemos f(x, y, z, t), donde (x, y, z) estan en
+# S2 \ {(0,0,-1)} y t en [0, 1].
 def transform(x, y, z, t):
-    l = (1 - t) + abs(-1 - z) * t
+    # abs(-1-z) = (1+z) en S2
+    l = (1 - t) + (1 + z) * t
     return x / l, y / l, -t + z * (1 - t)
 
 
+# Dado un tiempo t, una colección de puntos de la superficie
+# (x, y, z) y de la curva (x_, y_, z_) calculamos f_t para
+# cada uno de ellos.
 def animate(x, y, z, x_, y_, z_, t):
     xt, yt, zt = transform(x, y, z, t)
     x_t, y_t, z_t = transform(x_, y_, z_, t)
+
     ax = plt.axes(projection='3d')
+    # Fijamos los ejes para ver fielmente la deformación.
     ax.set_xlim3d(-1, 1)
     ax.set_ylim3d(-1, 1)
     ax.set_zlim3d(-1, 1)
+
+    # Pintamos la superficie deformada en tiempo t.
     ax.plot_surface(xt, yt, zt, rstride=1, cstride=1,
                     cmap='jet', edgecolor='none')
 
+    # Pintamos la curva gamma deformada en tiempo t.
     ax.plot(x_t, y_t, z_t, '-b', c="white", zorder=3)
     return ax,
 
 
+# Ejercicio 2.
 def exercise2():
+    # Parametrización discreta de la esfera.
     u = np.linspace(0, np.pi, 30)
     v = np.linspace(0, 2 * np.pi, 30)
 
@@ -146,6 +132,7 @@ def exercise2():
     y = np.outer(np.sin(u), np.cos(v))
     z = np.outer(np.cos(u), np.ones_like(v))
 
+    # Parametrización discreta de la curva gamma.
     t2 = np.linspace(-1, 0, 500)
 
     y2 = t2 * np.sin(127 * t2 / 2)
@@ -157,10 +144,11 @@ def exercise2():
     b2 = t2 * np.cos(127 * t2 / 2)
     z2 = np.concatenate((z2, -b2))
     x2 = np.concatenate((x2, -np.sqrt(1 - a2 ** 2 - b2 ** 2)))
-    c2 = x2 + y2
 
+    # Generamos la animacion.
     fig = plt.figure(figsize=(6, 6))
-    ani = animation.FuncAnimation(fig, lambda t: animate(x, y, z, x2, y2, z2, t),
+    ani = animation.FuncAnimation(fig,
+                                  lambda t: animate(x, y, z, x2, y2, z2, t),
                                   np.arange(0, 1, 0.0125), interval=80)
 
     ani.save("projected_sphere.gif", fps=10)
